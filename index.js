32e1548eccd407ec5ded1df736634baf360bfa82
Chanchalcr <chanchalrai193@gmail.com>
2022-06-24 10:03:55 +0530

    I improved the interface and add the user input functionality.

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
-let num1=8
-let num2=2
-document.getElementById("num1-el").textContent= num1
-document.getElementById("num2-el").textContent= num2
 
+let num1=document.getElementById("num1-el")
+let num2=document.getElementById("num2-el")
 let sum= document.getElementById("sum-el")
 
 function add() {
-    let sum1 = num1 + num2
-    sum.textContent = "Sum: " + sum1
+    let sum1 = parseInt(num1.value) + parseInt(num2.value)
+    sum.textContent = "Answer: " + sum1
 }
 
 function substract() {
-    let sum1 = num1 - num2
-    sum.textContent = "Sum: " + sum1
+    let sum1 = parseInt(num1.value) - parseInt(num2.value)
+    sum.textContent = "Answer: " + sum1
 }
 
 function multiply() {
-    let sum1 = num1 * num2
-    sum.textContent = "Sum: " + sum1
+    let sum1 = parseInt(num1.value) * parseInt(num2.value)
+    sum.textContent =  "Answer: " + sum1
 }
 
 function divide() {
-    let sum1 = num1 / num2
-    sum.textContent = "Sum: " + sum1
+    let sum1 = parseInt(num1.value) / parseInt(num2.value)
+    sum.textContent =  "Answer: " + sum1
 }
